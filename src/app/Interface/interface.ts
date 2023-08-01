d64ebe2c07c05c7833278917284dc1725b542548
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-08-01 19:54:25 +0530

    full last error

---
+++ src/app/Interface/interface.ts
@@ -0,0 +1,10 @@
+export interface Books{
+    image: string;
+    isbn13: string;
+    price: string;
+    subtitle:string;
+    title:string;
+    url:string;
+    quantity?:number;
+    addedToCart: boolean;
+}
